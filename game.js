417267e390fbbcfd666a1af0c5481131d4044c09
arthur <ganievart@gmail.com>
2019-06-06 09:30:15 +0200

    Add Start scene, index.html

---
+++ game.js
@@ -0,0 +1,20 @@
+const gameState = {
+	score: 0
+};
+
+const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 500,
+	backgroundColor: "b9eaff",
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+			enableBody: true,
+		}
+	},
+	scene: [StartScene]
+};
+
+const game = new Phaser.Game(config);
